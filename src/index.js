6bbf2662973caa8e5c7c7f58b9e2c29340ea004a
chanka-herath2001 <cb010779@students.apiit.lk>
2026-02-25 20:38:28 +0530

    removed / from url

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',   // Vite dev server
-    'https://spendguardian-frontend.vercel.app/',
+    'https://spendguardian-frontend.vercel.app',
   ],
   credentials: true,
 }));
